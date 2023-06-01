bbad1aa7ba4970819235fe9257e26797bdbb4ecf
yfbchen <yfbchen@gmail.com>
2023-05-31 20:47:22 -0400

    First draft with image uploads and slideshows and navbar

---
+++ src/pages/shop/styled.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+
+const SlideContainer = styled.div`
+    height: 400px;
+    width: 500px;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    height: 400px;
+    position: relative;
+`;
+
+export const Styled = {
+    SlideContainer,
+    Container,
+}
